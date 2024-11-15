69e82d1d30c7115ca69acdb2eebd582e7f9c71d3
arthenry90 <134339241+Arthenry90@users.noreply.github.com>
2024-11-15 20:06:51 -0300

    Finished array and loops exercices

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+ //c = (f-32) / 1,8;
+
+  let roundTemp = (temperature - 32) / 1.8
+
+  Math.round(roundTemp)
+  return roundTemp;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+//c * 1,8 + 32
+
+  let roundTemp = temperature * 1.8 + 32;
+  Math.round(roundTemp);
+  return roundTemp;
+
 };
 
 // Do not edit below this line
